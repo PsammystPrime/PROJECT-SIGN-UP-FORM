ddaeb04047ebc0d6f842637aae1012462db4ad0d
PsammystPrime <samuelprime384@gmail.com>
2023-12-28 20:04:16 +0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -4,7 +4,7 @@ const pass1 = document.querySelector('.error')
 const pass2 = document.querySelector('.error2')
 const email = document.getElementById("Email");
 
-email.addEventListener("input", (event) => {
+email.addEventListener("input", () => {
   if (email.validity.typeMismatch) {
     email.setCustomValidity("I am expecting an email address!");
   } else {
@@ -12,40 +12,20 @@ email.addEventListener("input", (event) => {
   }
 });
 
-submit.addEventListener('click', function (e) {
-    if (pass1.value1===pass2.value) {
+submit.addEventListener('click', function () {
+  
+   if (pass1.value1===pass2.value) {
         para.textContent= 'Registration Successful, welcome'
-   }
-   if (pass1.value1!==pass2.value) {
+   } else
+
+   if (pass1.value1 ==! pass2.value) {
          para.textContent= '*Passwords do not match'
-    }
+    } else
    if (pass1.value==='' && pass2.value==='') {
     para.textContent ='Fill all fields'
     }
+
 })
 
 
 
-if (title.value,author.value,pages.value!==''  ) {
-    const list1 =document.createElement('li')
-     const info = document.createElement('span')
-     const check = document.createElement('span')
-     const del = document.createElement('button')
-
-     info.textContent= title.value+' by '+author.value+ ' has '+pages.value+ ' pages.'          
-       if (status.checked) {
-         check.textContent= 'Read'
-       } else {
-         check.textContent = 'Unread'
-       };
-       del.textContent = 'delete'
-      del.addEventListener('click', ()=>{
-        bookList.removeChild(list1)
-      })
-
-     list1.appendChild(info),
-     list1.appendChild(check)
-     list1.appendChild(del),
-     bookList.appendChild(list1);
-       console.log('author.value')
-};
